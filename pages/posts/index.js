c5836dd0b46c93c65c5f26ee5da7d0a9cb0970b2
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-03-25 10:45:51 -0700

    delete dummy posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,41 +1,6 @@
 import AllPosts from "../../components/posts/AllPosts";
 import { getAllPosts } from "../../helpers/posts-util";
 
-// const DUMMY_POSTS = [
-//   {
-//     slug: "getting-started-with-nextjs1",
-//     title: "Getting Started with NextJS",
-//     image: "getting-started-nextjs.png",
-//     exceprt:
-//       "NextJS is a React framework for production - it makes building fullstack React simple",
-//     date: "2022-3-21",
-//   },
-//   {
-//     slug: "getting-started-with-nextjs2",
-//     title: "Getting Started with NextJS",
-//     image: "getting-started-nextjs.png",
-//     exceprt:
-//       "NextJS is a React framework for production - it makes building fullstack React simple",
-//     date: "2022-3-21",
-//   },
-//   {
-//     slug: "getting-started-with-nextjs3",
-//     title: "Getting Started with NextJS",
-//     image: "getting-started-nextjs.png",
-//     exceprt:
-//       "NextJS is a React framework for production - it makes building fullstack React simple",
-//     date: "2022-3-21",
-//   },
-//   {
-//     slug: "getting-started-with-nextjs4",
-//     title: "Getting Started with NextJS",
-//     image: "getting-started-nextjs.png",
-//     exceprt:
-//       "NextJS is a React framework for production - it makes building fullstack React simple",
-//     date: "2022-3-21",
-//   },
-// ];
-
 const AllPostPage = (props) => {
   const { allPosts } = props;
 
