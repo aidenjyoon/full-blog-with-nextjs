39a62c7d0eb64d45e05cbf7b935c41e2adf7c921
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-03-29 16:09:29 -0700

    code markdown styling

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import styles from "./PostContent.module.scss";
 import PostHeader from "./PostHeader";
@@ -40,6 +42,19 @@ const PostContent = (props) => {
         );
       }
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
