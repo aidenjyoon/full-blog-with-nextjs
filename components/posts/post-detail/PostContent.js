fb7a9fb9db67d1fb2badc5462942c087ce289516
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-03-29 11:12:46 -0700

    custom image components with ReactMarkdown
    use custom components for image to take advantage of the <Image> component from nextjs/image

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 
 import styles from "./PostContent.module.scss";
@@ -7,13 +8,50 @@ const PostContent = (props) => {
   const { post } = props;
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customComponents = {
+    // // to get rid of warnings caused by having <div>s in <p>
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+
+    // replace <p> with <div> to post image
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={styles.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <>
       <article className={styles.content}>
         <PostHeader title={post.title} image={imagePath} />
 
         {/* used ReactMarkdown to make content markdown formatted */}
-        <ReactMarkdown>{post.content}</ReactMarkdown>
+        <ReactMarkdown components={customComponents}>
+          {post.content}
+        </ReactMarkdown>
       </article>
     </>
   );
