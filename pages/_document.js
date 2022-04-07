35d53cf0b6a3508512c4c75943ce07f68fedb913
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-04-07 13:00:30 -0700

    additional meta data with _document.js
    create additional meta data "lang" with _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { render } from "react-dom";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
